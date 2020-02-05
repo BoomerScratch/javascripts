76259bd439f20409ee607870d5c6a363bc9405fc
Boomer001 <60622217+BoomerScratch@users.noreply.github.com>
2020-02-05 19:51:42 +0100

    Update PointerLock.js

--- PointerLock.js
+++ PointerLock.js
@@ -1,72 +1,44 @@
-var canvas = document.querySelector('canvas');
-var cursorlock
-class PointerLock {
-getInfo() {
-    return {
-        "id": "PointerLock",
-        "name": "PointerLock",
-        "blocks": [{
-                    "opcode": "cursorlockon",
-                    "blockType": "command",
-                    "text": "turn cursor lock on",
-                    "arguments": {}
-                },{
-                    "opcode": "cursorlockoff",
-                    "blockType": "command",
-                    "text": "turn cursor lock off",
-                    "arguments": {}
-                },{
-                    "opcode": "cursorlockmovementx",
-                    "blockType": "reporter",
-                    "text": "cursor lock movement X",
-                    "arguments": {}
-                },{
-                    "opcode": "cursorlockmovementy",
-                    "blockType": "reporter",
-                    "text": "cursor lock movement Y",
-                    "arguments": {}
-                },{
-                    "opcode": "cursorlockison",
-                    "blockType": "boolean",
-                    "text": "cursor lock is on?",
-                    "arguments": {}
-                }],
-        "menus": {}
-    };
-}
-cursorlockon({}) {
-var cursorlock = true
-}
-cursorlockoff({}) {
-var cursorlock = false
-}
-cursorlockmovementx({}) {
-function getMovement(e) {
-var mX = e.MovementX
-var mY = e.MovementY
-}
-return mX
-}
-cursorlockmovementy({}) {
-function getMovement(e) {
-var mX = e.MovementX
-var mY = e.MovementY
-}
-return mY
-}
-cursorlockison({}) {
-return cursorlock
-}
-}
-if (cursorlock) {
-canvas.requestPointerLock = canvas.requestPointerLock ||
-                            canvas.mozRequestPointerLock;
+(function(ext) {
+    ext._shutdown = function() {};
 
-canvas.onclick = function() {
-  canvas.requestPointerLock();
-}
-} else {
-document.exitPointerLock = document.exitPointerLock ||
-                           document.mozExitPointerLock;
-}
-Scratch.extensions.register(new PointerLock());
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+
+ext.pointerlockon = function(){
+var pointerlock = true
+canvas.requestPointerLock();
+};
+
+ext.pointerlockoff = function(){
+var pointerlock = false
+document.exitPointerLock();
+};
+
+ext.pointerlockmovementX = function(){
+return function xmove(e){return e.movementX}
+};
+
+ext.pointerlockmovementY = function(){
+return function ymove(e){return e.movementY}
+};
+
+ext.pointerlockison = function(){
+return pointerlock
+};
+
+    var descriptor = {
+        blocks: [
+[' ','turn pointer lock on','pointerlockon'],
+[' ','turn pointer lock off','pointerlockoff'],
+['r','pointer lock movement X','pointerlockmovementX'],
+['r','pointer lock movement Y','pointerlockmovementY'],
+['b','pointer lock is on?','pointerlockison'],
+        ]
+    };
+
+    ScratchExtensions.register('Cursor lock (hide cursor) by Boomer001', descriptor, ext);
+x=document.createElement("canvas");document.getElementsByTagName("head")[0].appendChild(x)
+var canvas = document.querySelector('canvas')
+var pointerlock = false
+})({});
