ada5a66c981ef0fe30376b1d6f97d6457b60c738
Boomer001 <60622217+BoomerScratch@users.noreply.github.com>
2020-02-06 20:31:03 +0100

    Update PointerLock.js

--- PointerLock.js
+++ PointerLock.js
@@ -14,37 +14,60 @@ document.exitPointerLock();
 };
 
 ext.pointerlockmovementX = function(){
-canvas.addEventListener("mousemove", e => {
-  const {
-    movementX,
-    movementY
-  } = e;
-    var mX = movementX
-})
-                        return mX
+   function changeCallback(e) {
+               if (document.pointerLockElement === canvas ||
+                document.mozPointerLockElement === canvas ||
+                document.webkitPointerLockElement === canvas) {
+            document.addEventListener("mousemove", updatePosX, false);
+        } else {
+            document.removeEventListener("mousemove", updatePosX, false);
+        }
+   }
+    function updatePosX(e) {
+      var movementX = e.movementX
+      }
+    return movementX
 };
 
 ext.pointerlockmovementY = function(){
-canvas.addEventListener("mousemove", e => {
-  const {
-    movementX,
-    movementY
-  } = e;
-    var mY = movementY
-})
-                        return mY
+   function changeCallback(e) {
+               if (document.pointerLockElement === canvas ||
+                document.mozPointerLockElement === canvas ||
+                document.webkitPointerLockElement === canvas) {
+            document.addEventListener("mousemove", updatePosY, false);
+        } else {
+            document.removeEventListener("mousemove", updatePosY, false);
+        }
+   }
+    function updatePosY(e) {
+      var movementY = e.movementY
+      }
+    return movementY
 };
-
+    
+ext.pointerlockison = function(){
+            if (document.pointerLockElement === canvas ||
+                document.mozPointerLockElement === canvas ||
+                document.webkitPointerLockElement === canvas) {
+return true
+        } else {
+return false
+        }
+};
+    
     var descriptor = {
         blocks: [
 [' ','request pointer lock','pointerlockon'],
 [' ','exit pointer lock','pointerlockoff'],
 ['r','pointer lock movement X','pointerlockmovementX'],
 ['r','pointer lock movement Y','pointerlockmovementY'],
+['b','pointer lock is on?','pointerlockison'],
         ]
     };
 
     ScratchExtensions.register('Pointer lock', descriptor, ext);
+    function setup() {document.addEventListener('pointerlockchange', changeCallback, false)}
+    setup()
 x=document.createElement("canvas");document.getElementsByTagName("head")[0].appendChild(x)
 var canvas = document.querySelector('canvas')
 })({});
