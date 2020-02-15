64bbc91f73015ce9707d41faf9e31a665ef53130
Boomer001 <60622217+BoomerScratch@users.noreply.github.com>
2020-02-15 12:00:03 +0100

    Add files via upload

---
+++ scratchdarktheme.js
@@ -0,0 +1,12 @@
+var all = document.getElementsByClassName('gui_menu-bar-position_3U1T0 menu-bar_menu-bar_JcuHF box_box_2jjDp');
+for (var i = 0; i < all.length; i++) {
+  all[i].style.backgroundColor = '#000000';
+};
+document.getElementsByClassName('blocklyMainBackground')[0].parentNode.removeChild(document.getElementsByClassName('blocklyMainBackground')[0]);
+document.getElementsByClassName('blocklySvg')[0].style.backgroundColor = '#000000'
+document.getElementsByClassName('sprite-info_sprite-info_3EyZh box_box_2jjDp')[0].style.backgroundColor = '#505050'
+document.getElementsByClassName('blocklyToolboxDiv')[0].style.backgroundColor = '#505050'
+document.getElementsByClassName('scratchCategoryMenu')[0].style.backgroundColor = '#505050'
+document.getElementsByClassName('scratchCategoryMenuItem scratchCategoryId-motion categorySelected')[0].style.backgroundColor = '#505050'
+document.getElementsByClassName('stage-selector_stage-selector_3oWOr box_box_2jjDp')[0].style.backgroundColor = '#505050'
+document.getElementsByClassName('stage-selector_header_2GVr1')[0].style.backgroundColor = '#505050'
