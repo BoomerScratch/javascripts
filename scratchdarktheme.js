b8116e8a13b76e6863b57b4f929d3fcfab43748b
Boomer001 <60622217+BoomerScratch@users.noreply.github.com>
2020-02-15 12:49:05 +0100

    Update scratchdarktheme.js

--- scratchdarktheme.js
+++ scratchdarktheme.js
@@ -10,6 +10,6 @@ document.getElementsByClassName('scratchCategoryMenu')[0].style.backgroundColor 
 document.getElementsByClassName('scratchCategoryMenuItem scratchCategoryId-motion categorySelected')[0].style.backgroundColor = '#505050'
 document.getElementsByClassName('stage-selector_stage-selector_3oWOr box_box_2jjDp')[0].style.backgroundColor = '#505050'
 document.getElementsByClassName('stage-selector_header_2GVr1')[0].style.backgroundColor = '#505050'
-document.getElementsByClassName('paint-editor_editor-container_1WLch')[0].style.backgroundColor = '#000000'
-document.getElementsByClassName('sound-editor_editor-container_iUSW-')[0].style.backgroundColor = '#000000'
+if (document.getElementsByClassName('paint-editor_editor-container_1WLch')[0] == undefined == false) {document.getElementsByClassName('paint-editor_editor-container_1WLch')[0].style.backgroundColor = '#000000'}
+if (document.getElementsByClassName('sound-editor_editor-container_iUSW-')[0] == undefined == false) {document.getElementsByClassName('sound-editor_editor-container_iUSW-')[0].style.backgroundColor = '#000000'}
 document.getElementsByClassName('blocklyMainBackground')[0].parentNode.removeChild(document.getElementsByClassName('blocklyMainBackground')[0]);},1)
