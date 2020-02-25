cb59c7eedd964632c01b54ea7bbd496a1873626a
Boomer001 <60622217+BoomerScratch@users.noreply.github.com>
2020-02-25 17:47:10 +0100

    Update speechrecognitionScratchX.js

--- speechrecognitionScratchX.js
+++ speechrecognitionScratchX.js
@@ -10,14 +10,20 @@
 
 ext.speechrecognitionstart = function(){
 recognition.start()
+var isonspeechrecognition = true
 }
 
 ext.speechrecognitionstop = function(){
 recognition.stop()
+var isonspeechrecognition = false
 }
 
 ext.speechrecognitionspoken = function(){
 return document.getElementById('recognitiontext').innerText
+}
+    
+ext.onspeechrecognition = function(){
+return isonspeechrecognition
 }
     // Block and block menu descriptions
     var descriptor = {
@@ -25,6 +31,7 @@ return document.getElementById('recognitiontext').innerText
 [" ","start speech recognition","speechrecognitionstart"],
 [" ","stop speech recognition","speechrecognitionstop"],
 ["r","spoken","speechrecognitionspoken"],
+["b","on speech recognition?","onspeechrecognition"],
         ],
     };
 
@@ -33,9 +40,11 @@ return document.getElementById('recognitiontext').innerText
 var elem = document.createElement('h6')
 elem.id = 'recognitiontext'
 document.getElementsByTagName('body')[0].appendChild(elem);
+var isonspeechrecognition = false
 var recognition = new webkitSpeechRecognition();
 recognition.onresult = function(e) {
 document.getElementById('recognitiontext').innerText = e.results[0][0].transcript;
 recognition.stop()
+var isonspeechrecognition = false
 };
 })({});
